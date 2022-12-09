e6e53f4fc9f23d1c9651724e812e32fa25508a4c
taek <c3334943@naver.com>
2022-12-09 15:58:29 +0900

    Initial commit

---
+++ src/main/java/PonyLand/PonyLand/PonyLandApplication.java
@@ -0,0 +1,13 @@
+package PonyLand.PonyLand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PonyLandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PonyLandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PonyLand/PonyLand/PonyLandApplicationTests.java
@@ -0,0 +1,13 @@
+package PonyLand.PonyLand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PonyLandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

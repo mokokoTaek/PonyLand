a27299d889b493418aa9268dd020697f64b4f6f9
taek <c3334943@naver.com>
2022-12-12 10:43:01 +0900

    jpa start

--- src/main/java/PonyLand/PonyLand/dao/MemberDAO.java
+++ src/main/java/PonyLand/PonyLand/dao/MemberDAO.java
@@ -1,5 +1,12 @@
 package PonyLand.PonyLand.dao;
 
+import PonyLand.PonyLand.Repository.SpringDataJpaMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class MemberDAO {
 
+    @Autowired
+    private SpringDataJpaMemberRepository sdjr;
 }

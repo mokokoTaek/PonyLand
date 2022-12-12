b81160c352705f3b0f57dd7c2cd8d60c32cdcc42
taek <c3334943@naver.com>
2022-12-12 14:02:21 +0900

    template

--- src/main/java/PonyLand/PonyLand/service/MemberService.java
+++ src/main/java/PonyLand/PonyLand/service/MemberService.java
@@ -1,14 +1,30 @@
 package PonyLand.PonyLand.service;
 
+import PonyLand.PonyLand.dao.MemberDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/member/")
+import java.util.Random;
+
+@Service
 public class MemberService {
 
-    @GetMapping
-    public void getWave(){
+    @Autowired
+    private MemberDAO dao;
 
+    @Autowired
+    private Random random;
+
+    public Long getWave(){
+        return dao.countMember();
     }
 
+    public int getRandom(Long countMember){
+
+        int intCountMember = countMember.intValue();
+        return (int) (Math.random() * intCountMember + 1);
+
+    }
 }

22668741da2c82f7479f6e3c590ab306cbf71807
이진혁 <dldlwls12346@naver.com>
2022-12-13 11:38:19 +0900

    Album crd

---
+++ src/main/java/PonyLand/PonyLand/controller/AlbumCommentController.java
@@ -0,0 +1,24 @@
+package PonyLand.PonyLand.controller;
+
+import PonyLand.PonyLand.dto.AlbumCommentDTO;
+import PonyLand.PonyLand.service.AlbumCommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Comment/")
+public class AlbumCommentController {
+
+    @Autowired
+    private AlbumCommentService service;
+
+    @RequestMapping("write")
+    public String write(AlbumCommentDTO dto) {
+        service.insert(dto);
+        return "album";
+    }
+
+
+
+}
---
+++ src/main/java/PonyLand/PonyLand/dto/AlbumCommentDTO.java
@@ -0,0 +1,23 @@
+package PonyLand.PonyLand.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AlbumCommentDTO {
+
+    private Integer Album_Comment_seq;
+    private String Album_Comment_writer;
+    private String Album_Comment_contents;
+    private Timestamp Album_Comment_write_date;
+    private Integer Album_Comment_parent_seq;
+
+
+}

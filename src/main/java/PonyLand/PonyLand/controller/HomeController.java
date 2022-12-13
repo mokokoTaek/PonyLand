f00e8fd3a0e672544a9d9cc3ad6425ac9eea28fd
taek <c3334943@naver.com>
2022-12-13 14:51:11 +0900

    template11

--- src/main/java/PonyLand/PonyLand/controller/HomeController.java
+++ src/main/java/PonyLand/PonyLand/controller/HomeController.java
@@ -1,6 +1,7 @@
 package PonyLand.PonyLand.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,7 +14,12 @@ public class HomeController {
     }
 
     @GetMapping("/toMiniPage")
-    public String miniHome(String id){ return "main?id="+id+""; }
+    public String miniHome(String id, Model model){
+        model.addAttribute("id",id);
+
+        return "main";
+    }
+
 
     @GetMapping("/toAlbumPage")
     public String AlbumPage(){

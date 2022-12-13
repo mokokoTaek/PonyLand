8e99e3dd18beefdee91095e1d0b72210cbf0a3ce
YHH <dbgksgh98@nave.com>
2022-12-13 10:43:33 +0900

    Guestbook commit 13

--- src/main/java/PonyLand/PonyLand/controller/GuestbookCommentController.java
+++ src/main/java/PonyLand/PonyLand/controller/GuestbookCommentController.java
@@ -1,5 +1,6 @@
 package PonyLand.PonyLand.controller;
 
+import PonyLand.PonyLand.dto.GuestbookCommentDTO;
 import PonyLand.PonyLand.service.GuestbookCommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,6 +16,10 @@ public class GuestbookCommentController {
 
 
     @RequestMapping("insert")
-    public String
+    public String insert(GuestbookCommentDTO dto)throws Exception{
+        service.insert(dto);
+        System.out.println(dto.getGuestbook_comment_contents());
+        return"";
+    }
 
 }

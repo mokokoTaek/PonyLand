e2d9ed59647c00eb5793808cb9d6e9ada0899370
taek <c3334943@naver.com>
2022-12-15 09:01:13 +0900

    template11

--- src/main/java/PonyLand/PonyLand/controller/HomeController.java
+++ src/main/java/PonyLand/PonyLand/controller/HomeController.java
@@ -1,12 +1,18 @@
 package PonyLand.PonyLand.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class HomeController {
+
+    @Autowired
+    private HttpSession session;
 
     @RequestMapping("/")
     public String home(){
@@ -16,7 +22,7 @@ public class HomeController {
     @GetMapping("/toMiniPage")
     public String miniHome(String id, Model model){
         model.addAttribute("id",id);
-
+        model.addAttribute("sessionID",session.getAttribute("sessionID"));
         return "main";
     }
 
@@ -36,4 +42,6 @@ public class HomeController {
         return "login";
     }
 
+    @GetMapping("/toFamilyOpen")
+    public String toFamilyOpen(){return "familyOpen";}
 }

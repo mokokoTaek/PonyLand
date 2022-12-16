a05a5555cc95fe47505a7bbbef7ff72d7621d01d
이진혁 <dldlwls12346@naver.com>
2022-12-16 09:03:12 +0900

    Album crd

--- src/main/java/PonyLand/PonyLand/controller/AlbumController.java
+++ src/main/java/PonyLand/PonyLand/controller/AlbumController.java
@@ -33,6 +33,9 @@ public class AlbumController {
 
 
 
+
+
+
 //    }
 
     @RequestMapping("write")
--- src/main/java/PonyLand/PonyLand/service/AlbumImagesService.java
+++
@@ -1,8 +0,0 @@
-//package PonyLand.PonyLand.service;
-//
-//
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class AlbumImagesService {
-//}

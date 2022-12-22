e5929f3387c40401ab424df5328dd9188b9085be
YHH <dbgksgh98@nave.com>
2022-12-22 14:18:27 +0900

    commit46

--- src/main/java/PonyLand/PonyLand/controller/GameController.java
+++ src/main/java/PonyLand/PonyLand/controller/GameController.java
@@ -25,6 +25,7 @@ public class GameController {
     private GameService Gameservice;
 
 
+    // 경마장으로 가는 코드
     @RequestMapping("gameStart")
     public String gameStart() {
         return "gameStart";
@@ -37,13 +38,14 @@ public class GameController {
         model.addAttribute("dto", dto);
         return "gameSetting";
     }
-
-
+    
+    //게임 설명 버튼 클릭시 게임설명 페이지 이동
     @RequestMapping("goGameExplain")
     public String gameExplain() throws Exception {
         return "gameExplain";
     }
 
+    // 게임 실행 버튼 클릭시  그게 맞는 말 번호 실행 페이지로 이동
     @RequestMapping("gameRun")
     public String gameRun(String id, int bettingCoin, int horseCount, String betHorse) throws Exception{
         System.out.println(bettingCoin + id + betHorse);
@@ -62,19 +64,21 @@ public class GameController {
     }
 
 
+    //게임 후 레이싱 결과화면으로 가는 코드
     @RequestMapping("goGameResult")
     public String goGameResult(String winner, Model model) throws Exception{
         String betNumber="";
         RacingDTO dto = new RacingDTO();
         dto = service.selectBet((String) session.getAttribute("sessionID"));
 
+        //내가 배팅한 말 번호랑 , 승리한 말이랑 번호가 같을 경우 업데이트문 실행, 업데이트 문 실행 후 racing 테이블  dto delete 문 실행
         if (dto.getRacing_horse_seq().equals(winner)) {
             System.out.println("d여긴?");
             service.updateWin(dto);
             betNumber = dto.getRacing_horse_seq();
             service.deleteBet((String) session.getAttribute("sessionID"));
 
-
+            //내가 배팅한 말 번호랑 , 승리한 말이랑 번호가 다를 경우 업데이트문 실행, 업데이트 문 실행 후 racing 테이블  dto delete 문 실행
         } else {
             System.out.println("d여긴??!?!??");
             service.updateLose(dto);
@@ -84,7 +88,7 @@ public class GameController {
         }
         System.out.println("위너 : " + winner);
 
-        // 결과 화면에 값 뿌려주기 위한 코드
+        // 결과 화면에 승리 말 값이랑 배팅한 말 번호 가져가기 위한 코드
         String member_id = (String) session.getAttribute("sessionID");
         MemberDTO dto1 = service.findById(member_id);
         model.addAttribute("dto", dto1);
